3552d09cf4dfae404ce68240ea8fdfc9471d0f67
Carlos <c.martinez@esciencecenter.nl>
2017-11-28 18:14:02 +0100

    Document

--- convergence.py
+++ convergence.py
@@ -17,13 +17,13 @@ from w2vtransformation import calculateTransform, getModelInv
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
 def computeConvergenceOverYearRange(yearRange, batchSize, maxSentences):
-    """Compute the convergence of w2v models generated over a given range of
+    '''Compute the convergence of w2v models generated over a given range of
     years. W2v models are generated for every batch of data produced by
     a SentenceLoader (using the given batchSize). Every model is compared
     with the previous one and the convergence between these models is
     calculated. This process stops once all years in the given range have been
     processed or once the maximum number of sentences (maxSentences) has been
-    reached."""
+    reached.'''
     # Initialize model
     vocabSizeMB = 1000 * 1024 * 1024
     model = gensim.models.Word2Vec(max_vocab_size=vocabSizeMB, size=300)
@@ -76,33 +76,63 @@ def computeConvergenceOverYearRange(yearRange, batchSize, maxSentences):
 
 
 def testModelConvergence(A, Ainv, B, Binv, vector_size):
-    """Given two models (A and B) and their inverse matrices, calculate the
-    convergence between the two models (as a single value)."""
+    '''Given two models (A and B) and their inverse matrices, calculate the
+    convergence between the two models (as a single value).'''
     TTinv = _calculateSymetricTransform(A, Ainv, B, Binv)
     Si_ip = np.trace(TTinv) / vector_size
     return Si_ip
 
-def generateModelsForPeriod(years, minSentences, maxSentences, nSteps, chunkSize, randomSeed, vector_size, modelFolder):
+
+def generateModelsForPeriod(years, minSentences, maxSentences, nSteps, chunkSize,
+                            randomSeed, vector_size, modelFolder, fixedAlpha=False):
+    '''Generates sets of w2v models trained using randomly selected sentences
+    from the given year period. Models are trained with an increasing number of
+    sentences, starting with minSentences and going up to maxSentences (in nSteps,
+    so nSteps models are generated).
+
+    randomSeed is used to initialize the random number generator.
+    vector_size defines the number of dimensions used by the w2v models.
+    modelFolder indicates the directory where the models will be saved.
+    fixedAlpha can be set to True to use a fixed learning rate for w2v.
+
+    Files generated in modelFolder:
+     - nSentences.pkl -- list containing the number of sentences to be used in each batch.
+     - year_nSentences.w2v -- the models (e.g. 1990_000100000000.w2v) (and corresponding vocab)
+    '''
+    # Build chunks to be used (if needed)
     checkPath(modelFolder)
     for year in years:
         buildChunks(year, chunkSize)
 
-    batchSizes = np.logspace(np.log10(minSentences), np.log10(maxSentences), num=nSteps, dtype=int)
+    # Initialize loader
     loader = RandomSentenceLoader(years, chunkSize=chunkSize, seed=randomSeed)
 
+    # The number of sentences to be used in each batch.
+    batchSizes = np.logspace(np.log10(minSentences), np.log10(
+        maxSentences), num=nSteps, dtype=int)
     pkl.dump(batchSizes, open(modelFolder + '/nSentences.pkl', 'w'))
 
     for batchSize in batchSizes:
-        cumSentencesStr = ('%d'%batchSize).zfill(12)
-        modelKey = '%d_'%years[0] + cumSentencesStr
+        cumSentencesStr = ('%d' % batchSize).zfill(12)
+        modelKey = '%d_' % years[0] + cumSentencesStr
 
-        modelName = modelFolder + '/%s.w2v'%(modelKey)
-        vocabName = modelName.replace('.w2v','.vocab.w2v')
-        print 'Building model: ',modelName
+        modelName = modelFolder + '/%s.w2v' % (modelKey)
+        vocabName = modelName.replace('.w2v', '.vocab.w2v')
+        print 'Building model: ', modelName
 
+        # Initialize w2v model
         vocabSizeMB = 1000 * 1024 * 1024
-        model = gensim.models.Word2Vec(max_vocab_size=vocabSizeMB, seed=randomSeed, size=vector_size)
+        if fixedAlpha:
+            print '...using fixed alpha...'
+            model = gensim.models.Word2Vec(
+                max_vocab_size=vocabSizeMB, seed=randomSeed, size=vector_size, alpha=0.75, min_alpha=0.75)
+        else:
+            model = gensim.models.Word2Vec(
+                max_vocab_size=vocabSizeMB, seed=randomSeed, size=vector_size)
 
+        # build vocabulary, then train the model with same sentences.
+        # We use a generator to avoid loading all sentences at once, but that
+        # means in order to use the same set of sentences, we reset loader.
         loader.reset()
         batch = loader.nextBatchGenerator(batchSize=batchSize)
         model.build_vocab(batch)
@@ -115,8 +145,10 @@ def generateModelsForPeriod(years, minSentences, maxSentences, nSteps, chunkSize
         model.wv.save_word2vec_format(modelName, fvocab=vocabName, binary=True)
 
 
-
-def computeConvergenceOverYearRangeWithBuildModels(modelFolder, vector_size):
+def computeConvergenceOverYearRangeWithBuiltModels(modelFolder, vector_size):
+    '''Use w2v models on modelFolder (created via generateModelsForPeriod),
+    compute the model convergence.'''
+    # *.w2v files, ignoring *.vocab.w2v files.
     files = sorted(glob(modelFolder + '/*[!vocab].w2v'))
 
     convergence = SortedDict()
@@ -126,13 +158,16 @@ def computeConvergenceOverYearRangeWithBuildModels(modelFolder, vector_size):
     oldModel, oldModelInv = None, None
     for f in files:
         print '... loading file: ', f.replace(modelFolder, '')
-        newModel = gensim.models.KeyedVectors.load_word2vec_format(f, binary=True)
+        newModel = gensim.models.KeyedVectors.load_word2vec_format(
+            f, binary=True)
         newModel.init_sims(replace=False)
         newModelInv = getModelInv(newModel)
 
-        cumSentences = int(f.replace(modelFolder, '').replace('.w2v', '').split('_')[1])
+        cumSentences = int(f.replace(modelFolder, '').replace(
+            '.w2v', '').split('_')[1])
         if oldModel is not None:
-            d = testModelConvergence(oldModel, oldModelInv, newModel, newModelInv, vector_size)
+            d = testModelConvergence(
+                oldModel, oldModelInv, newModel, newModelInv, vector_size)
             convergence[cumSentences] = d
 
         vocabSize[cumSentences] = len(newModel.vocab)
@@ -142,17 +177,18 @@ def computeConvergenceOverYearRangeWithBuildModels(modelFolder, vector_size):
 
     return convergence, sentenceYearCounter, vocabSize
 
+
 def measureDiagonalConvergence(A, Ainv, B, Binv):
-    """Given two models (A and B) and their inverse matrices, calculate the
+    '''Given two models (A and B) and their inverse matrices, calculate the
     convergence between the two models, per dimension (as a vector of the same
-    size as the dimensions of the models)."""
+    size as the dimensions of the models).'''
     TTinv = _calculateSymetricTransform(A, Ainv, B, Binv)
     return TTinv.diagonal()
 
 
 def _calculateSymetricTransform(A, Ainv, B, Binv):
-    """Given two models (A and B) calculate the symetric version of the
-    transformation matrix."""
+    '''Given two models (A and B) calculate the symetric version of the
+    transformation matrix.'''
     Tab = calculateTransform(A, Ainv, B, Binv, sameVocab=False)
     Tba = calculateTransform(B, Binv, A, Ainv, sameVocab=False)
     TTinv = Tab * Tba
@@ -160,7 +196,7 @@ def _calculateSymetricTransform(A, Ainv, B, Binv):
 
 
 def _makeCopy(model):
-    """Make a copy of the given w2v model."""
+    '''Make a copy of the given w2v model.'''
     _, fname = tempfile.mkstemp()
     model.save(fname)
     newModel = gensim.models.Word2Vec.load(fname)
--- randomsentenceloader.py
+++ randomsentenceloader.py
@@ -11,9 +11,20 @@ import logging
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
+
 def getChunks():
+    '''Generate a list of chunks for each year available on the chunkCache.
+    Lists of all available chunks are stored in a dictionary with years as its keys.
+    A 'chunk' is a pklz file containing N sentences from a given year.
+
+    e.g:
+    chunks = {
+        1900: [ '1900_s1000_b000001.pklz', '1900_s1000_b000002.pklz' ]
+        1910: [ '1910_s1000_b000001.pklz', '1910_s1000_b000002.pklz' ]
+    }
+    '''
     chunks = glob(chunkCache + '*.pklz')
-    chunks = [ chunk.replace(chunkCache, '') for chunk in chunks ]
+    chunks = [chunk.replace(chunkCache, '') for chunk in chunks]
 
     year_chunk = defaultdict(list)
     for chunk in chunks:
@@ -23,47 +34,81 @@ def getChunks():
 
     return year_chunk
 
+
 def getChunkName(year, chunkSize, chunkNumber='*'):
-    return '%d_s%d_b%s.pklz'%(year,chunkSize, chunkNumber)
+    '''Build the name of a 'chunk' for a given year, size and chunk number.
+    The year indicates the year from which sentences in a chunk come from.
+    The chunkSize incidates the maximum number of sentences in the chunk.
+    The chunkNumber indicates is a consecutive number identifying the chunk.
+    If no chunkNumber is given, the name is generated with a '*' (which can
+    be use in a glob).
+    '''
+    return '%d_s%d_b%s.pklz' % (year, chunkSize, chunkNumber)
+
 
 def buildChunks(year, chunkSize):
+    '''Loads all sentences from a given year and splits them in chuncks
+    of the given number of sentences (chunkSize). The number of chunks depends on
+    the total number of sentences available. E.g. for a year containing 5123
+    sentences, with a chunk size of 1000, 6 chunks will be generated (5 chunks
+    with 1000 sentences and one chunk with 123).'''
     existingChunks = glob(chunkCache + getChunkName(year, chunkSize))
-    if len(existingChunks)>0:
-        print 'Chunks for %d have already been created. Skipping...'%year
+    if len(existingChunks) > 0:
+        print 'Chunks for %d have already been created. Skipping...' % year
         return
 
     n0 = 0
     sentences = getSentencesForYear(year)
-    r = range(chunkSize,len(sentences),chunkSize) + [ len(sentences) ]
-    for i,n1 in enumerate(r):
+    r = range(chunkSize, len(sentences), chunkSize) + [len(sentences)]
+    for i, n1 in enumerate(r):
         sentence_chunk = sentences[n0:n1]
         n0 = n1
         chunkName = getChunkName(year, chunkSize, chunkNumber=str(i).zfill(6))
         fileName = chunkCache + chunkName
         with gzip.open(fileName, 'wb') as f:
-                pkl.dump(sentence_chunk, f)
+            pkl.dump(sentence_chunk, f)
 
 
 class RandomSentenceLoader():
+    '''Loads batches of sentences from a range of years at random.
+
+    E.g: Given the years with available chunks.
+    1900: [ c1900.1, c1900.2, c1900.3, c1900.4, c1900.5, c1900.6 ]
+    1901: [ c1901.1, c1901.2, c1901.3, c1901.4, c1901.5, c1901.6 ]
+    1902: [ c1902.1, c1902.2, c1902.3, c1902.4, c1902.5, c1902.6 ]
+
+    A batch could be: [ c1900.3, c1902.1, c1900.5, c1901.5 ]. Where c190?.? is
+    a list of sentences.
+    '''
+
     def __init__(self, years, chunkSize=1000, seed=90517):
+        '''Initialize sentence loader.
+        Years is the range of years from which sentences can be loaded. chunkSize
+        is the numbe of sentences on each chunk. seed is used to initialize random
+        number generator (so randomness can be reproducible). This is necessary
+        in order to produce two random sets which are random, but identical to
+        each other.'''
         self.seed = seed
         self.years = years
         self.chunkSize = chunkSize
         self.reset()
 
     def reset(self):
+        '''Reset random number generator to initial state.'''
         np.random.seed(self.seed)
         chunks = getChunks()
-        self.chunks = { y: chunks[y] for y in self.years }
+        self.chunks = {y: chunks[y] for y in self.years}
 
     def _nextRandomYear(self):
-        if len(self.chunks)==0:
+        '''Select year from which next chunk will be taken'''
+        if len(self.chunks) == 0:
             return None
         nextYear = np.random.choice(self.chunks.keys())
         log.debug('Next year: %d', nextYear)
         return nextYear
 
-    def _nextMiniBatchName(self):
+    def _nextChunkName(self):
+        '''Select the filename of the next chunk to be loaded'''
         nextYear = self._nextRandomYear()
         if nextYear is None:
             return None
@@ -73,26 +118,30 @@ class RandomSentenceLoader():
         miniBatch = yearChunks[selectIdx]
         del yearChunks[selectIdx]
 
-        if len(yearChunks)==0:
+        if len(yearChunks) == 0:
             del self.chunks[nextYear]
         return miniBatch
 
-    def _nextMiniBatch(self):
-        fileName = self._nextMiniBatchName()
+    def _nextChunk(self):
+        '''Loads the next chunk of sentences.'''
+        fileName = self._nextChunkName()
         if fileName is None:
             return []
         with gzip.open(chunkCache + fileName, 'rb') as f:
             return pkl.load(f)
 
-
     def nextBatch(self, batchSize=2000):
+        '''Generate a batch of sentences of the given batchSize. Sentences are
+        selected (in chunks) from different years at random.'''
         batch = []
         for i in range(0, batchSize, self.chunkSize):
-            batch += self._nextMiniBatch()
+            batch += self._nextChunk()
         return batch
 
     def nextBatchGenerator(self, batchSize=2000):
+        '''As nextBatch, but returns a generator (so the full batch is not
+        loaded into memory at once).'''
         for i in range(0, batchSize, self.chunkSize):
-            batch = self._nextMiniBatch()
+            batch = self._nextChunk()
             for b in batch:
                 yield b
